bdda3d63a47613b8f1cff3ec875aa04bbb24f6f3
thelllounge <46868624+thelllounge@users.noreply.github.com>
2024-05-29 22:57:39 +0900

    try and get things started

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
 
-hello_world()
 
 if __name__ == "__main__":
     app.run()
