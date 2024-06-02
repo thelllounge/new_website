0a0e516a8796ec348f2bceff8528e21a16a5b29d
thelllounge <46868624+thelllounge@users.noreply.github.com>
2024-06-02 15:55:28 +0900

    add all bases for html pages and make navbar work

--- main.py
+++ main.py
@@ -8,5 +8,39 @@ def index():
     return render_template('index.html')
 
 
+# Writing Stuff
+@app.route("/prompt_generator")
+def prompt_generator():
+    return render_template('prompt_generator.html')
+
+
+@app.route("/sprint_timer")
+def sprint_timer():
+    return render_template('sprint_timer.html')
+
+
+@app.route("/title_generator")
+def title_generator():
+    return render_template('title_generator.html')
+
+
+@app.route("/wb_breaker")
+def wb_breaker():
+    return render_template('wb_breaker.html')
+
+# Random Coding Stuff
+@app.route("/snake_game")
+def snake_game():
+    return render_template('snake_game.html')
+
+@app.route("/language_typing")
+def language_typing():
+    return render_template('language_typing.html')
+
+
+@app.route("/language_mtg")
+def language_mtg():
+    return render_template('language_mtg.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
