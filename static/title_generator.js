3dfa4d9136ccee6bab9f9b46d8b1d4460e43c772
thelllounge <46868624+thelllounge@users.noreply.github.com>
2024-06-16 12:31:49 +0900

    Worked on Title generator

--- static/title_generator.js
+++ static/title_generator.js
@@ -1,6 +1,29 @@
-let promptPosition1 = ["Power", "Lives", "Reign"]
-let promptPosition2 = ["Dragons", "Night", "the Sea"]
+let promptPosition1Options = ["Power", "Lives", "Reign", "Court", "Dance", "Night"]
+let promptPosition2Options = ["Dragons", "Night", "the Sea", "Love", "Death"]
+let promptPosition3Options = ["Died", "Conquered", "Won", "Lost", "Was Silenced"]
+
+function get_prompt_items() {
+    promptPosition1 = promptPosition1Options[Math.floor(Math.random() * promptPosition1Options.length)]
+    promptPosition2 = promptPosition2Options[Math.floor(Math.random() * promptPosition2Options.length)]
+    promptPosition3 = promptPosition3Options[Math.floor(Math.random() * promptPosition3Options.length)]
+    return [promptPosition1, promptPosition2, promptPosition3]
+}
+
 
 function get_title() {
-    document.getElementById("title").innerHTML = `The ${promptPosition1[Math.floor(Math.random() * promptPosition1.length)]} of ${promptPosition2[Math.floor(Math.random() * promptPosition2.length)]}`
+//    promptPosition1 = promptPosition1Options[Math.floor(Math.random() * promptPosition1Options.length)]
+//    promptPosition2 = promptPosition2Options[Math.floor(Math.random() * promptPosition2Options.length)]
+
+    let promptItems = get_prompt_items()
+    Position1 = promptItems[0]
+    Position2 = promptItems[1]
+    Position3 = promptItems[2]
+
+    prompts = [`The ${Position1} of ${Position2}`,
+                `The ${Position1}`,
+                `The ${Position2}`,
+                `The Night ${Position2} ${Position3}`]
+
+    prompt = prompts[Math.floor(Math.random() * prompts.length)]
+    document.getElementById("title").innerHTML = prompt
 }
